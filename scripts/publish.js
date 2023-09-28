02022e4f5b83db356de0adb33e41ab4976f9d7f6
isaaxite <isaacgun@outlook.com>
2023-09-29 05:58:09 +0800

    ci: update workflow

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,9 +1,14 @@
+import { execSync } from 'child_process';
 import { getPublishCmdBy, getPkgData } from '@isubo-org/publish';
 
 async function main() {
   const { version } = getPkgData();
   const cmd = await getPublishCmdBy({ version });
   console.info(cmd);
+  execSync(cmd, {
+    shell: true,
+    stdio: 'inherit',
+  });
 }
 
 main();
